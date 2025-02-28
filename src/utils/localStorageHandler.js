9059e2f6f3768e156d4d101fe2889b37582cb960
Touseef870 <touseefabid47@gmail.com>
2025-02-28 13:44:36 -0800

    build error solve

--- src/utils/localStorageHandler.js
+++ src/utils/localStorageHandler.js
@@ -1,9 +1,23 @@
-'use client'
+// utils/localStorageHandler.js
+'use client';
 
 const localStorageHandler = {
-    set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
-    get: (key) => JSON.parse(localStorage.getItem(key)) || null,
-    remove: (key) => localStorage.removeItem(key)
+    set: (key, value) => {
+        if (typeof window !== "undefined") {
+            localStorage.setItem(key, JSON.stringify(value));
+        }
+    },
+    get: (key) => {
+        if (typeof window !== "undefined") {
+            return JSON.parse(localStorage.getItem(key)) || null;
+        }
+        return null;
+    },
+    remove: (key) => {
+        if (typeof window !== "undefined") {
+            localStorage.removeItem(key);
+        }
+    }
 };
 
 export default localStorageHandler;
